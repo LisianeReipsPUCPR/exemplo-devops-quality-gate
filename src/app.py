431da1f015d81dfff89534f419f4deb8e9294efb
lisiane <lisireips@gmail.com>
2026-05-22 11:00:28 -0300

    feat: adicionar pipeline com quality gate - cobertura alta

--- src/app.py
+++ src/app.py
@@ -1,6 +1,11 @@
 def somar(a, b):
     return a + b
 
-def dividir(a, b):
+#def dividir(a, b):
     # BUG intencional: sem tratamento de divisão por zero
+#    return a / b
+
+def dividir(a, b):
+    if b == 0:
+        raise ValueError("Erro")
     return a / b
