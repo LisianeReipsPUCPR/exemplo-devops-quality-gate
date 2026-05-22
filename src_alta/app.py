30d29058fca03de0b7cce06bb9a717a452278a26
lisiane <lisireips@gmail.com>
2026-05-22 12:21:00 -0300

    feat: adicionar pipelines de quality gate com cenários de cobertura

--- src_alta/app.py
+++ src_alta/app.py
@@ -1,11 +1,8 @@
 def somar(a, b):
     return a + b
 
-#def dividir(a, b):
-    # BUG intencional: sem tratamento de divisão por zero
-#    return a / b
-
 def dividir(a, b):
     if b == 0:
         raise ValueError("Erro")
     return a / b
+
